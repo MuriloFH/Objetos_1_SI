070738ef6871b167570a61c0455dcac4fe7db01d
Murilo Fernandes <murilo.fernandes@betha.com.br>
2023-05-28 18:19:57 -0300

    alterações

--- objetos_1/trabalho1_objetos/src/functions/Funcoes.java
+++ objetos_1/trabalho1_objetos/src/functions/Funcoes.java
@@ -14,10 +14,38 @@ public class Funcoes {
             salario *= horaMes;
         }
 
-        salario = abstracts.Dados_abstratos.getDescontoINSS(salario);
+        salario = getDescontoINSS(salario);
 
-        salario = abstracts.Dados_abstratos.getDescontoIRRF(salario);
+        salario = getDescontoIRRF(salario);
 
         return Math.round(salario);
+    }
+    
+    public static double getDescontoINSS(double salario) {
+        if (salario <= 1320.00) {
+            salario = salario - (salario * 0.075);
+        } else if (salario > 1320.00 && salario <= 2571.29) {
+            salario = salario - (salario * 0.09);
+        } else if (salario > 2571.30 && salario <= 3856.94) {
+            salario = salario - (salario * 0.12);
+        } else {
+            salario = salario - (salario * 0.14);
+        }
+        return salario;
+    }
+
+    public static double getDescontoIRRF(double salario) {
+        if (salario <= 2112.00) {
+            return salario;
+        } else if (salario > 2112.00 && salario <= 2826.65) {
+            salario = salario - (salario * 0.075);
+        } else if (salario > 2826.65 && salario <= 3751.05) {
+            salario = salario - (salario * 0.15);
+        } else if (salario > 3751.05 && salario <= 4664.68) {
+            salario = salario - (salario * 0.225);
+        } else {
+            salario = salario - (salario * 0.275);
+        }
+        return salario;
     }
 }
